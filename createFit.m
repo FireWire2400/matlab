6d7d48e90fbdee2806a0cde9163f8ce95d3bc247
iiivanov <ivanoff.ilya@gmail.com>
2019-06-17 03:58:33 +0700

    coefficients setup

--- createFit.m
+++ createFit.m
@@ -1,6 +1,6 @@
 function [fitresult, gof] = createFit(num_exp, num, x, Source)
-  if num_exp==3
-     if num==1
+  if num_exp == 3
+     if num == 1
          [xData, yData] = prepareCurveData( x, Source );
          % Задание параметров
          ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))', 'independent', 'x', 'dependent', 'y' );
@@ -18,7 +18,7 @@ function [fitresult, gof] = createFit(num_exp, num, x, Source)
          xlabel t
          ylabel F(t)
          grid on
-     elseif num==2
+     elseif num == 2
          [xData, yData] = prepareCurveData( x, Source );
          % Задание параметров
          ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))', 'independent', 'x', 'dependent', 'y' );
@@ -36,7 +36,7 @@ function [fitresult, gof] = createFit(num_exp, num, x, Source)
          xlabel t
          ylabel F(t)
          grid on
-     elseif num==6
+     elseif num == 6
          [xData, yData] = prepareCurveData( x, Source );
          % Задание параметров
          ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))', 'independent', 'x', 'dependent', 'y' );
@@ -56,8 +56,8 @@ function [fitresult, gof] = createFit(num_exp, num, x, Source)
          grid on
      end
   end
-  if num_exp==4
-     if num==4
+  if num_exp == 4
+     if num == 4
          [xData, yData] = prepareCurveData( x, Source );
          % Задание параметров
          ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))+a4*exp(-(x-g4).^2.*(b4*(x<=g4)+c4*(x>g4)))', 'independent', 'x', 'dependent', 'y' );
@@ -75,14 +75,14 @@ function [fitresult, gof] = createFit(num_exp, num, x, Source)
          xlabel t
          ylabel F(t)
          grid on
-     elseif num==5
+     elseif num == 5
          [xData, yData] = prepareCurveData( x, Source );
          % Задание параметров
          ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))+a4*exp(-(x-g4).^2.*(b4*(x<=g4)+c4*(x>g4)))', 'independent', 'x', 'dependent', 'y' );
          opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
          opts.Display = 'Off';
          opts.Lower = [-1 -1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0];
-         opts.StartPoint = [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 25 60 70 150];
+         opts.StartPoint = [0 0 0 0 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 25 65 70 150];
          opts.Upper = [1 1 1 1 1 1 1 1 1 1 1 1 250 250 250 250];
          % Интерполяция
          [fitresult, gof] = fit( xData, yData, ft, opts );
@@ -93,7 +93,7 @@ function [fitresult, gof] = createFit(num_exp, num, x, Source)
          xlabel t
          ylabel F(t)
          grid on
-     elseif num==7
+     elseif num == 7
          [xData, yData] = prepareCurveData( x, Source );
          % Задание параметров
          ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))+a4*exp(-(x-g4).^2.*(b4*(x<=g4)+c4*(x>g4)))', 'independent', 'x', 'dependent', 'y' );
@@ -113,14 +113,14 @@ function [fitresult, gof] = createFit(num_exp, num, x, Source)
          grid on
      end
   end
-  if num_exp==5
+  if num_exp == 5
      [xData, yData] = prepareCurveData( x, Source );
      % Задание параметров
      ft = fittype( 'a1*exp(-(x-g1).^2.*(b1*(x<=g1)+c1*(x>g1)))+a2*exp(-(x-g2).^2.*(b2*(x<=g2)+c2*(x>g2)))+a3*exp(-(x-g3).^2.*(b3*(x<=g3)+c3*(x>g3)))+a4*exp(-(x-4).^2.*(b4*(x<=g4)+c4*(x>g4)))+a5*exp(-(x-g5).^2.*(b5*(x<=g5)+c5*(x>g5)))', 'independent', 'x', 'dependent', 'y' );
      opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
      opts.Display = 'Off';
      opts.Lower = [-1 -1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-     opts.StartPoint = [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 60 80 90 120 200];
+     opts.StartPoint = [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 60 70 90 120 200];
      opts.Upper = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 250 250 250 250 250];
      % Интерполяция
      [fitresult, gof] = fit( xData, yData, ft, opts );
--- mnk.m
+++ mnk.m
@@ -1,27 +1,30 @@
 clear, clc
+% Загрузка файлов с отсчетами сигнала ЭКГ
 load('A');load('B');load('C');load('D');load('E');load('F');load('G');
-Source=input('Введите сигнал для аппроксимации (A, B, C, D, E, F, G):\n');
-if Source==A
-    num=1;
-    num_exp=3;
-elseif Source==B
-    num=2;
-    num_exp=3;
-elseif Source==C
-    num=3;
-    num_exp=5;
-elseif Source==D
-    num=4;
-    num_exp=4;
-elseif Source==E
-    num=5;
-    num_exp=4;
-elseif Source==F
-    num=6;
-    num_exp=3;
-elseif Source==G
-    num=7;
-    num_exp=4;
+% Выбор сигнала
+Source = input('Введите сигнал для аппроксимации (A, B, C, D, E, F, G):\n');
+if Source == A
+    num = 1;
+    num_exp = 3;
+elseif Source == B
+    num = 2;
+    num_exp = 3;
+elseif Source == C
+    num = 3;
+    num_exp = 5;
+elseif Source == D
+    num = 4;
+    num_exp = 4;
+elseif Source == E
+    num = 5;
+    num_exp = 4;
+elseif Source == F
+    num = 6;
+    num_exp = 3;
+elseif Source == G
+    num = 7;
+    num_exp = 4;
 end
-x=1:250;
+x = 1:250;
+% Вызов функции аппроксимации
 Approx = createFit(num_exp, num, x, Source);
